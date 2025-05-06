853f12f4aeed4861c8638ddc66b3f2afcf4b5d4f
neuronflow <florian.kofler@tum.de>
2025-05-06 11:45:54 +0200

    clean and nicer warnings
    Signed-off-by: neuronflow <florian.kofler@tum.de>

--- main.py
+++ main.py
@@ -26,25 +26,17 @@ niftiConvert(input_dir=T2F_DCM_DIR, output_dir=T2F_NII_DIR)
 # now we check if only one nifti came out for each conversion
 T1N_file_List = T1N_NII_DIR.files("*.nii.gz")
 if len(T1N_file_List) != 1:
-    raise ValueError(
-        f"Expected exactly one T1N file, but found {len(T1N_file_List)} !"
-    )
-    
+    raise ValueError(f"Expected exactly one T1N file, but found {len(T1N_file_List)} !")
+
 T1C_file_List = T1C_NII_DIR.files("*.nii.gz")
 if len(T1C_file_List) != 1:
-    raise ValueError(
-        f"Expected exactly one T1C file, but found {len(T1C_file_List)} !"
-    )
+    raise ValueError(f"Expected exactly one T1C file, but found {len(T1C_file_List)} !")
 T2W_file_List = T2W_NII_DIR.files("*.nii.gz")
 if len(T2W_file_List) != 1:
-    raise ValueError(
-        f"Expected exactly one T2W file, but found {len(T2W_file_List)} !"
-    )
+    raise ValueError(f"Expected exactly one T2W file, but found {len(T2W_file_List)} !")
 T2F_file_List = T2F_NII_DIR.files("*.nii.gz")
 if len(T2F_file_List) != 1:
-    raise ValueError(
-        f"Expected exactly one T2F file, but found {len(T2F_file_List)} !"
-    )
+    raise ValueError(f"Expected exactly one T2F file, but found {len(T2F_file_List)} !")
 
 # define source nifti files in native space
 T1N_file = T1N_file_List[0]
@@ -52,7 +44,7 @@ T1C_file = T1C_file_List[0]
 T2W_file = T2W_file_List[0]
 T2F_file = T2F_file_List[0]
 
-# TODO do preprocessing and segmentation magic
+# TODO do preprocessing and segmentation magic here
 
 # TODO adjust paths to resulting segmentation files
 T1N_NII_segmentation_file = turbopath("TODO")
@@ -72,4 +64,3 @@ T1N_DCM_segmentation_file = turbopath("TODO")
 T1C_DCM_segmentation_file = turbopath("TODO")
 T2W_DCM_segmentation_file = turbopath("TODO")
 T2F_DCM_segmentation_file = turbopath("TODO")
-
